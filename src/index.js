89de098e82a0045053edaea95f4f24806adeee59
nakkk <nakkk@users.noreply.github.com>
2018-05-26 12:17:42 +0200

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const poWyslaniu = (e) => {
+    e.preventDefault();
+    alert('lol!');
+};
+
 const Login = (props) => {
     console.log(props);
     return (
-        <form>
+        <form onSubmit={props.onSubmit}>
             <input type="text" placeholder={props.inputPlaceholder}/>
             <button>{props.label}</button>
         </form>
@@ -12,6 +17,6 @@ const Login = (props) => {
 }
 
 ReactDOM.render(
-    <Login label="Zaloguj" inputPlaceholder="podaj login..."/>,
+    <Login label="Zaloguj" inputPlaceholder="podaj login..." onSubmit={poWyslaniu}/>,
     document.getElementById('root')
 );
