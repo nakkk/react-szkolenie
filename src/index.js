bac129d43fcc364c9b34c071576a8fe0368e1bc1
nakkk <nakkk@users.noreply.github.com>
2018-05-26 13:36:21 +0200

    5th commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const poWyslaniu = (e) => {
-    e.preventDefault();
-    alert('lol!');
+class Login extends React.Component {
+    static defaultProps = {
+        label: "Zaloguj",
+        inputPlaceholder: "Podaj login..."
+    };
+
+    state = {
+        user: null
+    };
+
+    render () {
+        const props = this.props;
+        console.log(props);
+        return (
+            <form onSubmit = {(e) => {
+                if(this.state.user) {
+                    alert(this.state.user)
+                }
+                e.preventDefault();
+            }}>
+                <input type="text"
+                    placeholder={props.inputPlaceholder}
+                    onChange = { e => {
+                        this.setState({
+                            user: e.target.value
+                        })
+                    }}
+                />
+                <button>{props.label}</button>
+            </form>
+        );
+    };
 };
 
-const Login = (props) => {
-    console.log(props);
-    return (
-        <form onSubmit={props.onSubmit}>
-            <input type="text" placeholder={props.inputPlaceholder}/>
-            <button>{props.label}</button>
-        </form>
-    );
-}
-
 ReactDOM.render(
-    <Login label="Zaloguj" inputPlaceholder="podaj login..." onSubmit={poWyslaniu}/>,
+    <Login />,
     document.getElementById('root')
 );
